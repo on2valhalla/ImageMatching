e5306e0bac995d9fee6685840c559b596b193cc6
Jason Carlisle Mann <m5praetor@gmail.com>
2013-03-10 12:11:59 -0400

    making headway

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -53,29 +53,9 @@ int MainWindow::init()
 	vector<Mat> histograms;
     getHistograms(images, histograms, BUCKETS, B_THRESH, W_THRESH);
 
-
-
 	//will hold the normalized L1 comparison data
-	double norms[NUM_IMAGES][NUM_IMAGES];
-
-	// organized by image, (max/min), (value/idx)
-    double localMaxMin[NUM_IMAGES][2][2];
+    Mat norms(NUM_IMAGES, NUM_IMAGES , CV_32F);
     for (int i = 0; i < NUM_IMAGES; i++)
-	{
-		//set initial values
-		localMaxMin[i][0][0] = 0;
-        localMaxMin[i][1][0] = 2;
-	}
-
-    // organized by (max/min), (value/idx1/idx2)
-    double globalMaxMinPair[2][3];
-    globalMaxMinPair[0][0] = 0;
-    globalMaxMinPair[1][0] = 2;
-
-
-
-    for (int i = 0; i < NUM_IMAGES; i++)
-	{
         for (int j = 0; j < NUM_IMAGES; j++)
 		{
 			// get the l1 norm of the two histograms
@@ -83,85 +63,48 @@ int MainWindow::init()
 
 			// map the value to [0,1] (1 is the same image)
             normVal = 1 - (normVal/2);
-            norms[i][j] = normVal;
+            norms.at<float>(i,j) = normVal;
+            //hack to make min max work
+            if( i == j)
+                norms.at<float>(i,j) = .1;
+        }
 
-			if (i == j)
-				continue;
 
-			if (normVal > localMaxMin[i][0][0])
-			{
-				localMaxMin[i][0][0] = normVal;
-				localMaxMin[i][0][1] = j;
-			}
-			if (normVal < localMaxMin[i][1][0])
-			{
-				localMaxMin[i][1][0] = normVal;
-				localMaxMin[i][1][1] = j;
-			}
-			if (normVal > globalMaxMinPair[0][0])
-			{
-				globalMaxMinPair[0][0] = normVal;
-				globalMaxMinPair[0][1] = i;
-				globalMaxMinPair[0][2] = j;
-			}
-			if (normVal < globalMaxMinPair[1][0])
-			{
-				globalMaxMinPair[1][0] = normVal;
-				globalMaxMinPair[1][1] = i;
-				globalMaxMinPair[1][2] = j;
-            }
-		}
-	}
-
+    //get display images
+    vector<QImage> qImages(NUM_IMAGES);
+    for (int i = 0; i< NUM_IMAGES; i++)
+        qImages[i] = QImage(fileNames[i].c_str());
 
 	QTextCursor curs = this->ui->textEdit->textCursor();
     curs.insertText("Color Histogram Matches");
 	curs.insertTable(40,3);
 	for (int i = 0; i< NUM_IMAGES; i++)
-	{
-		QImage qImage = QImage(fileNames[i].c_str());
-		curs.insertImage(qImage);
+    {
+        curs.insertImage(qImages[i]);
         curs.insertText("\n" + QString(fileNames[i].c_str()).section("/",-1));
 		curs.movePosition(QTextCursor::NextCell);
 
-        int maxIdx = (int) localMaxMin[i][0][1];
-        qImage = QImage(fileNames[maxIdx].c_str());
-		curs.insertImage(qImage);
-        curs.insertText("\n" + QString(fileNames[maxIdx].c_str()).section("/",-1));
+        //find max/min match  (idx: 0/1)
+        double *vals[2];
+        int idxs[2][2];
+        minMaxIdx(norms.col(i), vals[1], vals[0], idxs[1], idxs[0]);
+
+        curs.insertImage(qImages[idxs[0][0]]);
+        curs.insertText("\n" + QString(fileNames[idxs[0][0]].c_str()).section("/",-1)
+                + QString("\n%1").arg(*vals[0],4,'f',5));
 		curs.movePosition(QTextCursor::NextCell);
 
-        int minIdx = (int) localMaxMin[i][1][1];
-        qImage = QImage(fileNames[minIdx].c_str());
-		curs.insertImage(qImage);
-        curs.insertText("\n" + QString(fileNames[minIdx].c_str()).section("/",-1));
-		curs.movePosition(QTextCursor::NextCell);
-
+        curs.insertImage(qImages[idxs[1][0]]);
+        curs.insertText("\n" + QString(fileNames[idxs[0][0]].c_str()).section("/",-1)
+                + QString("\n%1").arg(*vals[1],4,'f',5));
+        curs.movePosition(QTextCursor::NextCell);
 	}
 
 
-
-	// cout << norms[0];
-	// vector<Mat> triples;
-	// for (int i = 0; i < 2; i ++)
-	// {
-	//    cout << i;
-	//    vector<Mat> tmp;
-	//    tmp.push_back(images[i]);
-	//    tmp.push_back(images[localMaxMin[i][0][1]]);
-	//    tmp.push_back(images[localMaxMin[i][1][1]]);
-	//    Mat combine = manyToOne(tmp, 1, 3);
-	//    triples.push_back(combine);
-	// }
-
-	// Mat bigTriples = manyToOne(triples, 10, 4);
-	// namedWindow("triples");
-	// imshow("triples", bigTriples);
-
-
-	// Mat bigImage = manyToOne(images, 10, 4);
-	// namedWindow("all");
-	// imshow("all", bigImage);
-	// waitKey(0); // Wait for a keystroke in the window
+	Mat bigImage = manyToOne(images, 10, 4);
+	namedWindow("all");
+	imshow("all", bigImage);
+//	waitKey(0); // Wait for a keystroke in the window
 
 	return 0;
 }
